c3e80756b4ca9a7e5693c76c96109db71cfb4e6b
KayayaYay <inf.lindow@gmail.com>
2024-07-22 16:55:38 +0200

    hologram added

---
+++ TechArtTest_KaiBennetLindow/Assets/Scripts/HologramMaterialReader.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace TechArtTest {
+    [RequireComponent(typeof(VisualEffect)), ExecuteAlways]
+    public class HologramMaterialReader : MonoBehaviour {
+        [SerializeField] private VisualEffect hologrammEffect;
+        public VisualEffect HologrammEffect => hologrammEffect;
+        
+        [SerializeField] private Transform rayOriginTransform;
+        public Transform RayOriginTransform => rayOriginTransform;
+
+        private void OnValidate() {
+            if (hologrammEffect == null) {
+                hologrammEffect = GetComponent<VisualEffect>();
+            }
+        }
+
+        private void Update() {
+            if (hologrammEffect == null) {
+                return;
+            }
+
+            SkinnedMeshRenderer skinnedMeshRenderer = hologrammEffect.GetSkinnedMeshRenderer("SkinnedMeshRenderer");
+            if (skinnedMeshRenderer == null || skinnedMeshRenderer.sharedMaterial == null) {
+                return;
+            }
+
+            Texture albedoTexture = skinnedMeshRenderer.sharedMaterial.GetTexture("_Albedo");
+            if (albedoTexture != null) {
+                hologrammEffect.SetTexture("SkinnedMeshAlbedoTexture", albedoTexture);
+            }
+            
+            hologrammEffect.SetVector4("SkinnedMeshAlbedoTint", skinnedMeshRenderer.sharedMaterial.GetColor("_AlbedoTint"));
+
+            if (rayOriginTransform != null) {
+                hologrammEffect.SetVector3("RayOrigin", rayOriginTransform.position);
+            }
+        }
+    }
+}
